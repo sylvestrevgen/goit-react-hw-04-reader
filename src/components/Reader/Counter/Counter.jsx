afb6425a9a774e6ed44b39ee25de56264d213e2f
sylvestrevgen <sylvestrevgen@gmail.com>
2019-10-03 21:31:30 +0300

    replace browserrouter with hashrouter cause gh pages

--- src/components/Reader/Counter/Counter.jsx
+++ src/components/Reader/Counter/Counter.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './counter.module.css';
 
-const Counter = ({ index, sum }) => {
-  return <p className={styles.counter}>{`${Number(index) + 1}/${sum}`}</p>;
-};
+const Counter = ({ index, sum }) => (
+  <p className={styles.counter}>{`${Number(index) + 1}/${sum}`}</p>
+);
 
 Counter.propTypes = {
   index: PropTypes.number.isRequired,
--- src/components/Reader/Publication/Publication.jsx
+++ src/components/Reader/Publication/Publication.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './publication.module.css';
 
-const Publication = ({ item: { title, text } }) => {
-  return (
-    <article className={styles.publication}>
-      <h2 className={styles.header}>{title}</h2>
-      <p className={styles.content}>{text}</p>
-    </article>
-  );
-};
+const Publication = ({ item: { title, text } }) => (
+  <article className={styles.publication}>
+    <h2 className={styles.header}>{title}</h2>
+    <p className={styles.content}>{text}</p>
+  </article>
+);
 
 Publication.propTypes = {
   item: PropTypes.shape({
